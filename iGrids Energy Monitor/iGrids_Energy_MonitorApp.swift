db2cfe22d7762f69651dc85aeb352800044563d1
Infinite Grids Inc <schiu@infinitegrids.com>
2022-02-22 07:39:41 -0500

    Initial Commit

---
+++ iGrids Energy Monitor/iGrids_Energy_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iGrids_Energy_MonitorApp.swift
+//  iGrids Energy Monitor
+//
+//  Created by Steven Chiu on 2022-02-22.
+//
+
+import SwiftUI
+
+@main
+struct iGrids_Energy_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
